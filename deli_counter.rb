7979ab889fdcf58942dae5330493343d53217c5a
Milan <milan.dhar@gmail.com>
2019-03-01 16:33:05 -0800

    deli methods

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def line(array)
+  if (array.length == 0)
+   puts "The line is currently empty."
+  else
+    empty = []
+    counter = 0
+    while counter < array.length
+      empty << ("#{counter+1}. #{array[counter]}")
+      counter += 1
+    end
+    puts "The line is currently: #{empty.join(" ")}"
+  end
+end
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if (deli.length == 0)
+   puts "There is nobody waiting to be served!"
+ else
+  puts "Currently serving #{deli.shift}."
+  end
+end
